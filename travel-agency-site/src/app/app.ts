2e30420a3f1af282ec4f8a506de506d271f527b0
Arun Upadhyay <ace.arunkumar.nic@gmail.com>
2026-02-14 04:25:44 +0530

    Add responsive mobile hamburger menu and auto scroll-to-top on page navigation

--- travel-agency-site/src/app/app.ts
+++ travel-agency-site/src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, signal, OnInit } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('travel-agency-site');
+  protected readonly mobileMenuOpen = signal(false);
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Scroll to top on route change
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo(0, 0);
+      });
+  }
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen.update(current => !current);
+  }
+
+  closeMobileMenu() {
+    this.mobileMenuOpen.set(false);
+  }
 }
