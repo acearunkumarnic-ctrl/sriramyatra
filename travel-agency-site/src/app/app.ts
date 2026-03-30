29cc616209b509b9807b39c857f0c72bfbaa829a
Arun Upadhyay <ace.arunkumar.nic@gmail.com>
2026-03-31 00:52:03 +0530

    Add logo page loader overlay on every route navigation

--- travel-agency-site/src/app/app.ts
+++ travel-agency-site/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal, OnInit, Inject, PLATFORM_ID } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd, NavigationStart, NavigationCancel, NavigationError } from '@angular/router';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { filter } from 'rxjs/operators';
 
@@ -12,10 +12,28 @@ import { filter } from 'rxjs/operators';
 export class App implements OnInit {
   protected readonly title = signal('travel-agency-site');
   protected readonly mobileMenuOpen = signal(false);
+  protected readonly pageLoading = signal(false);
 
   constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.pageLoading.set(true);
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        // Small delay so the loader is visible even on fast navigations
+        if (isPlatformBrowser(this.platformId)) {
+          setTimeout(() => this.pageLoading.set(false), 400);
+        } else {
+          this.pageLoading.set(false);
+        }
+      }
+    });
+
     // Scroll to top on route change
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
